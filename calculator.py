3ea2d058d0cdac3b80803867cf20b2078c60ba96
Luke Rider <luke@rider.us>
2018-07-22 20:40:37 -0700

    implement calculator functionality

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 For your homework this week, you'll be creating a wsgi application of
 your own.
@@ -42,43 +43,111 @@ To submit your homework:
 """
 
 
-def add(*args):
-    """ Returns a STRING with the sum of the arguments """
-
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
-
-    return sum
-
-# TODO: Add functions for handling more arithmetic operations.
-
 def resolve_path(path):
     """
     Should return two values: a callable and an iterable of
     arguments.
     """
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
+    funcs = {
+        '' : show_help,
+        'add' : add,
+        'subtract' : subtract,
+        'multiply' : multiply,
+        'divide' : divide,
+    }
+
+    path = path.strip('/').split('/')
+    args = path[1:]
+    
+    try:
+        func = funcs[path[0]]
+    except KeyError:
+        raise NameError
 
     return func, args
 
+
+def show_help(*args):
+    page = """
+Usage: Construct slash-separated URI with operation and arguments.  E.g.,<br />
+<a href='multiply/3/5'>multiply/3/5</a><br />
+<a href='add/23/42'>add/23/42</a><br />
+<a href='subtract/23/42'>subtract/23/42</a><br />
+<a href='divide/22/11'>divide/22/11/a><br />
+"""
+    return page.format()
+
+
+def add(*args):
+    page = """{}"""
+
+    sum = int(args[0])
+    for arg in args[1:]:
+        sum += int(arg)
+
+    return page.format(sum)
+
+
+def subtract(*args):
+    page = """{}"""
+
+    difference = int(args[0])
+    for arg in args[1:]:
+        difference -= int(arg)
+
+    return page.format(difference)
+
+
+def multiply(*args):
+    page = """{}"""
+
+    product = int(args[0])
+    for arg in args[1:]:
+        product *= int(arg)
+
+    return page.format(product)
+
+
+def divide(*args):
+    page = """{}"""
+
+    quotient = int(args[0])
+    for arg in args[1:]:
+        quotient /= int(arg)
+
+    return page.format(quotient)
+
+
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+    headers = [("Content-type", "text/html")]
+    status = "500 Internal Server Error"
+    body = "<h1>Internal Server Error</h1>"
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError as e:
+        print(e)
+        print(traceback.format_exc())
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except ZeroDivisionError as e:
+        status = "400 Bad Request"
+        body = "<h1>Attempted to divide by zero</h1>"
+    except Exception as e:
+        print(e)
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
+
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
